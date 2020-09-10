2dbeaf335c428c006e6e7e5d53cf2c242cb8d98c
angelina-tsuboi <angelina.t1832@gmail.com>
2020-09-10 11:14:04 -0700

    created employee layer

--- src/main/java/com/udacity/jdnd/course3/critter/entity/Employee.java
+++ src/main/java/com/udacity/jdnd/course3/critter/entity/Employee.java
@@ -25,5 +25,28 @@ public class Employee {
     @OneToMany
     @JoinColumn(name = "skills")
     private Set<DayOfWeek> daysAvailable;
+
+    public void setId(Long id){
+        this.id = id;
+    }
+    public void setName(String name){this.name = name;}
+
+    public void setSkills(Set<EmployeeSkill> skills){this.skills = skills;}
+
+    public void setDaysAvailable(Set<DayOfWeek> daysAvailable){this.daysAvailable = daysAvailable;}
+
+    public String getName(){return name;}
+
+    public Long getId() {
+        return id;
+    }
+
+    public Set<DayOfWeek> getDaysAvailable() {
+        return daysAvailable;
+    }
+
+    public Set<EmployeeSkill> getSkills() {
+        return skills;
+    }
 }
 
--- src/main/java/com/udacity/jdnd/course3/critter/service/EmployeeService.java
+++ src/main/java/com/udacity/jdnd/course3/critter/service/EmployeeService.java
@@ -1,4 +1,32 @@
 package com.udacity.jdnd.course3.critter.service;
 
+import com.udacity.jdnd.course3.critter.entity.Employee;
+import com.udacity.jdnd.course3.critter.entity.Hamster;
+import com.udacity.jdnd.course3.critter.repository.EmployeeRepository;
+import com.udacity.jdnd.course3.critter.repository.PetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmployeeService {
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    public Long save(Employee employee){
+        employeeRepository.persist(employee);
+        return employee.getId();
+    }
+
+    public Long update(Employee employee){
+        employeeRepository.merge(employee);
+        return employee.getId();
+    }
+
+    public void delete(Long id){
+        employeeRepository.delete(id);
+    }
+
+    public Employee get(Long id){
+        return employeeRepository.find(id);
+    }
 }
