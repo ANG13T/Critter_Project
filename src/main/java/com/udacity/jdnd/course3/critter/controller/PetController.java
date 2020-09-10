f02e7c8a131cdc2a853a926a118f9e6e16443f5c
angelina-tsuboi <angelina.t1832@gmail.com>
2020-09-10 11:34:40 -0700

    complete pet controller

--- src/main/java/com/udacity/jdnd/course3/critter/controller/PetController.java
+++ src/main/java/com/udacity/jdnd/course3/critter/controller/PetController.java
@@ -1,4 +1,34 @@
 package com.udacity.jdnd.course3.critter.controller;
 
+import com.udacity.jdnd.course3.critter.entity.Hamster;
+import com.udacity.jdnd.course3.critter.service.PetService;
+import com.udacity.jdnd.course3.critter.service.PetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/pet")
 public class PetController {
+    @Autowired
+    private PetService petService;
+
+    @GetMapping("/{id}")
+    public Hamster getPet(@PathVariable Long id) {
+        return petService.get(id);
+    }
+
+    @PostMapping
+    public void createPet(Hamster pet) {
+        petService.save(pet);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePet(@PathVariable Long id) {
+        petService.delete(id);
+    }
+
+    @PatchMapping
+    public void updatePet(Hamster pet){
+        petService.update(pet);
+    }
 }
