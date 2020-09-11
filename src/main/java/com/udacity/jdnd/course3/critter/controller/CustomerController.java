3561f9674256a25ff54f01038aad8abd8939ed92
angelina-tsuboi <angelina.t1832@gmail.com>
2020-09-10 20:02:30 -0700

    done with controller layer

--- src/main/java/com/udacity/jdnd/course3/critter/controller/CustomerController.java
+++ src/main/java/com/udacity/jdnd/course3/critter/controller/CustomerController.java
@@ -1,4 +1,34 @@
 package com.udacity.jdnd.course3.critter.controller;
 
+import com.udacity.jdnd.course3.critter.entity.Customer;
+import com.udacity.jdnd.course3.critter.service.CustomerService;
+import com.udacity.jdnd.course3.critter.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/customer")
 public class CustomerController {
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping("/{id}")
+    public Customer getCustomer(@PathVariable Long id) {
+        return customerService.get(id);
+    }
+
+    @PostMapping
+    public void createCustomer(Customer customer) {
+        customerService.save(customer);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCustomer(@PathVariable Long id) {
+        customerService.delete(id);
+    }
+
+    @PatchMapping
+    public void updateCustomer(Customer customer){
+        customerService.update(customer);
+    }
 }
--- src/main/java/com/udacity/jdnd/course3/critter/controller/EmployeeController.java
+++ src/main/java/com/udacity/jdnd/course3/critter/controller/EmployeeController.java
@@ -1,4 +1,34 @@
 package com.udacity.jdnd.course3.critter.controller;
 
+import com.udacity.jdnd.course3.critter.entity.Employee;
+import com.udacity.jdnd.course3.critter.service.EmployeeService;
+import com.udacity.jdnd.course3.critter.service.PetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/employee")
 public class EmployeeController {
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/{id}")
+    public Employee getEmployee(@PathVariable Long id) {
+        return employeeService.get(id);
+    }
+
+    @PostMapping
+    public void createEmployee(Employee employee) {
+        employeeService.save(employee);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteEmployee(@PathVariable Long id) {
+        employeeService.delete(id);
+    }
+
+    @PatchMapping
+    public void updateEmployee(Employee employee){
+        employeeService.update(employee);
+    }
 }
